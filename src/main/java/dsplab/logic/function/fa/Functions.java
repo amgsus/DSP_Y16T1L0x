e917893eefa605f1f9519e573ed6c053268b98ab
A.G <me@amgsus.net>
2016-11-14 02:29:14 +0300

    Add sawtooth function to the factory

--- src/main/java/dsplab/logic/function/fa/Functions.java
+++ src/main/java/dsplab/logic/function/fa/Functions.java
@@ -3,7 +3,8 @@ package dsplab.logic.function.fa;
 import dsplab.logic.function.Function;
 import dsplab.logic.function.impl.CosineFunction;
 import dsplab.logic.function.impl.SineFunction;
-import dsplab.logic.function.impl.noise.NoiseFunction;
+import dsplab.logic.function.impl.extended.SawtoothFunction;
+import dsplab.logic.function.impl.extended.NoiseFunction;
 import dsplab.logic.signal.enums.Waveform;
 
 public final class Functions implements FunctionFactory
@@ -23,6 +24,8 @@ public final class Functions implements FunctionFactory
                 return CosineFunction.getInstance();
             case Noise:
                 return NoiseFunction.getInstance();
+            case Sawtooth:
+                return SawtoothFunction.getInstance();
 
             default:
                 final String s = "'%s' is not supported by the factory";
