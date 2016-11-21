f53ab431716c2ea37c9d21bfc95058d8f05060ab
A.G <me@amgsus.net>
2016-11-21 03:24:18 +0300

    Implement LogarithmicValueModifier

--- src/main/java/dsplab/logic/gen/modifier/impl/concrete/LogarithmicValueModifier.java
+++ src/main/java/dsplab/logic/gen/modifier/impl/concrete/LogarithmicValueModifier.java
@@ -7,32 +7,15 @@ import static dsplab.logic.gen.modifier.alg.ValueModifierAlgorithm.LOGARITHMIC;
 
 public class LogarithmicValueModifier extends AbstractValueModifier
 {
-    public LogarithmicValueModifier() {}
-
-    // -------------------------------------------------------------------- //
-
-    @Override
-    public double[] getAllValues(int size, double startValue,
-        double finalValue)
-    {
-        double[] c = new double[size];
-
-        /*
-        final double step = (startCoefficient + finalCoefficient) / size;
-        double offset = 0; // Replace multiplication with sum
-
-        for (int i = 0; i < c.length; i++) {
-            c[i] = startCoefficient + offset;
-            offset = offset + step;
-        }
-        */
-
-        return c;
-    }
-
     @Override
     public ValueModifierAlgorithm getScale()
     {
         return LOGARITHMIC;
+    }
+
+    @Override
+    public double[] getAllValues(int tickCount, int ticksPerPeriod)
+    {
+        throw new RuntimeException("Not implemented");
     }
 }
